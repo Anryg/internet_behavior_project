fce55d6e2bdd687c1958f7a474ec8988e9afd96c
zhangqinhe <anrygzhang@gmail.com>
2022-08-31 15:51:10 +0800

    第一次提交代码

---
+++ spark-coding/src/main/scala/com/anryg/bigdata/hive/ConnectHive.scala
@@ -0,0 +1,34 @@
+package com.anryg.bigdata.hive
+
+import org.apache.spark.SparkConf
+//import org.apache.spark.sql.SparkSession
+
+/**
+  * @DESC:
+  * @Auther: Anryg
+  * @Date: 2022/4/8 16:30
+  */
+/*object ConnectHive {
+
+    def main(args: Array[String]): Unit = {
+        val conf = new SparkConf()
+        conf.setAppName("connect_hive")
+        val sparkSession = SparkSession.builder().config(conf)
+                //.config("spark.sql.warehouse.dir","hdfs://192.168.211.106:8020/warehouse/tablespace/managed/hive")
+                //.config("spark.sql.hive.hiveserver2.jdbc.url","jdbc:hive2://hdp01.pcl-test.com:2181,hdp03.pcl-test.com:2181,hdp02.pcl-test.com:2181/;serviceDiscoveryMode=zooKeeper;zooKeeperNamespace=hiveserver2")
+                //.config("spark.datasource.hive.warehouse.metastoreUri","thrift://hdp01.pcl-test.com:9083")
+                .enableHiveSupport()
+                .getOrCreate()
+
+        val result = sparkSession.sql("select * from xas.as_bgp_bak limit 3")
+        result.show()
+
+        sparkSession.close()
+        sparkSession.stop()
+    }
+}*/
+
+
+/*val hive = HiveWarehouseSession.session(sparkSession).build()//获取HWC对象
+        val result = hive.executeQuery("select * from doi_data limit 2")//查询hive表数据
+        result.show()*/
